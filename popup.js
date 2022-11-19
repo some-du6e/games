295b9e6e39300ad06f1b942dca73e7fe7770a8a3
some-du6e <karim2012eltaib@gmail.com>
2022-11-19 10:44:20 -0600

    d

--- popup.js
+++ popup.js
@@ -1,65 +1,61 @@
-versionrn = 2
+versionrn = 2;
 function sleep(milliseconds) {
-   const date = Date.now();
-   let currentDate = null;
-   do {
-      currentDate = Date.now();
-   } while (currentDate - date < milliseconds);
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
 }
 function writecss(css) {
-   var styleSheet = document.createElement("style")
-   styleSheet.innerText = css
-   document.head.appendChild(styleSheet)
+  var styleSheet = document.createElement("style");
+  styleSheet.innerText = css;
+  document.head.appendChild(styleSheet);
 }
 
-
-function httpGet(url) {
-   fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
-                    .then(response => {
-                      if (response.ok) return response.json()
-                      throw new Error('Network response was not ok.')
-                    })
-                    .then(data => console.log(data.contents));
-                  
+//function to get plain text from a site
+function httpGet(theUrl) {
+  var xmlHttp = new XMLHttpRequest();
+  var realurl = "https://uncors.vercel.app?url=" + theUrl;
+  xmlHttp.open("GET", realurl, false); // false for synchronous request
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
 }
-
-
 
 function isnewversion() {
-   var get = httpGet("https://some-du6e.github.io/games/version")
+  var get = httpGet("https://some-du6e.github.io/games/version");
 
-   console.log(get)
-   if (versionrn == get) {
-      return true
-   } else {
-      return false
-   }
+  console.log(get);
+  if (versionrn == get) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function hasinternet() {
-   var get = httpGet("https://some-du6e.github.io/games/internetcheck")
-   var get2 = get
-   console.log(get2)
-   sleep(500)
-   console.log(get2)
-   if (get2 == "true") {
-      get = true
-   } else {
-      get2 = false
-   }
-}
-function getridofupdatescreen() {
-   var elem = document.getElementById("");
-   elem.parentNode.removeChild(elem);
-}
-function main() {
-   const fullscreenthing = document.getElementById("fullscreenthing")
-   // chrome-extension://gbdmgmjeepkkgimbolaljoimengoiife/src/html/popup.html
-   fullscreenthing.href = "chrome-extension://" + chrome.runtime.id + "/popup.html"
-   isnewversion()
-   httpGet("https://some-du6e.github.io/games/api/internetcheck")
+  var get = httpGet("https://some-du6e.github.io/games/internetcheck");
+  if (get == "true") {
+    get = true;
+  } else {
+    get = false;
+  }
+  return get;
 }
 
-// writecss("#updatescreen {display: block !important}")
-// writecss("#menu {display: none !important}")
-main()
+function toggleupdatescreen() {
+  if (
+    document.getElementById("updatescreeninupdatescreen").style.display ==
+    "block"
+  ) {
+    document.getElementById("updatescreeninupdatescreen").style.display =
+      "none";
+  } else {
+    document.getElementById("updatescreeninupdatescreen").style.display =
+      "block";
+  }
+}
+const fullscreenthing = document.getElementById("fullscreenthing");
+// chrome-extension://gbdmgmjeepkkgimbolaljoimengoiife/src/html/popup.html
+fullscreenthing.href =
+  "chrome-extension://" + chrome.runtime.id + "/popup.html";
+isnewversion()
