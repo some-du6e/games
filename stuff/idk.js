1ad8a213fc0149b3405ad5d0dc7a3fcbe31b6e18
some-du6e <103129948+some-du6e@users.noreply.github.com>
2022-11-23 16:39:16 +0000

    ndjkskasdkda

--- stuff/idk.js
+++ stuff/idk.js
@@ -3,25 +3,28 @@
 document.head = document.head || document.getElementsByTagName('head')[0];
 
 function changeFavicon(src) {
- var link = document.createElement('link'),
-     oldLink = document.getElementById('dynamic-favicon');
- link.id = 'dynamic-favicon';
- link.rel = 'shortcut icon';
- link.href = src;
- if (oldLink) {
-  document.head.removeChild(oldLink);
- }
- document.head.appendChild(link);
+  var link = document.createElement('link'),
+    oldLink = document.getElementById('dynamic-favicon');
+  link.id = 'dynamic-favicon';
+  link.rel = 'shortcut icon';
+  link.href = src;
+  if (oldLink) {
+    document.head.removeChild(oldLink);
+  }
+  document.head.appendChild(link);
 }
 function prodigymode() {
   if (window.prodigymodetoggle == true) {
     document.title = "some-du6e games";
     window.prodigymodetoggle = false;
     changeFavicon("")
-  }else if (window.prodigymodetoggle == null || window.prodigymodetoggle == false) {
+  } else if (window.prodigymodetoggle == null || window.prodigymodetoggle == false) {
     document.title = "Play Prodigy";
     window.prodigymodetoggle = true;
     changeFavicon("https://play.prodigygame.com/images/favicon-06c858cf91b0d230aad7.ico");
   }
+}
 
+function openawesomewebversion() {
+  // fix
 }
