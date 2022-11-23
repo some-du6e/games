6a7f5dc79140d33926a56a64d3476baa4fdb146a
some-du6e <karim2012eltaib@gmail.com>
2022-11-22 20:10:58 -0600

    sasasa

--- stuff/idk.js
+++ stuff/idk.js
@@ -1,10 +1,27 @@
-document.getElementById("cards").onmousemove = e => {
-  for(const card of document.getElementsByClassName("card")) {
-    const rect = card.getBoundingClientRect(),
-          x = e.clientX - rect.left,
-          y = e.clientY - rect.top;
+// some-du6e 2022
+// Path: stuff/other.js
+document.head = document.head || document.getElementsByTagName('head')[0];
 
-    card.style.setProperty("--mouse-x", `${x}px`);
-    card.style.setProperty("--mouse-y", `${y}px`);
-  };
+function changeFavicon(src) {
+ var link = document.createElement('link'),
+     oldLink = document.getElementById('dynamic-favicon');
+ link.id = 'dynamic-favicon';
+ link.rel = 'shortcut icon';
+ link.href = src;
+ if (oldLink) {
+  document.head.removeChild(oldLink);
+ }
+ document.head.appendChild(link);
+}
+function prodigymode() {
+  if (window.prodigymodetoggle == true) {
+    document.title = "some-du6e games";
+    window.prodigymodetoggle = false;
+    changeFavicon("")
+  }else if (window.prodigymodetoggle == null || window.prodigymodetoggle == false) {
+    document.title = "Play Prodigy";
+    window.prodigymodetoggle = true;
+    changeFavicon("https://play.prodigygame.com/images/favicon-06c858cf91b0d230aad7.ico");
+  }
+
 }
