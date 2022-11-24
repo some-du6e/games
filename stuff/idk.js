6237d5bd54ac3b205edcee94d9b286147d43f98b
some-du6e <103129948+some-du6e@users.noreply.github.com>
2022-11-23 18:55:23 -0600

    mfw about:blank

--- stuff/idk.js
+++ stuff/idk.js
@@ -26,5 +26,36 @@ function prodigymode() {
 }
 
 function openawesomewebversion() {
-  // fixx
+    var redirect = "https://google.com"
+    
+      function inFrame() {
+          try {
+              return window.self !== window.top;
+          } catch (e) {
+              return true;
+          }
+      }
+      
+      function usingFirefox() {
+          return navigator.userAgent.indexOf("Firefox") != -1;
+      }
+      var redirectSite = "https://www.google.com";
+      if (!inFrame() && usingFirefox() != true) {
+          var tab = window.open('about:blank', '_blank');
+          if (!tab || tab.closed || typeof tab.closed == 'undefined') {
+              alert("get rid of ur popup blocker");
+              if (confirm("do you want to get the unsafe version? (visible in the history)")) { 
+                  window.location.href = "https://some-du6es-alt.github.io/games/popup.html"
+              }
+          } else {
+              if (window.location.pathname != "/") {
+                  tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Canvas</title><link rel="icon" type="image/png" href="https://du11hjcvx0uqb.cloudfront.net/dist/images/favicon-e10d657a73.ico"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + '?url=' + window.location.pathname + '" frameborder="0"></iframe></body></html>';
+              } else {
+                  tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Canvas</title><link rel="icon" type="image/png" href="https://du11hjcvx0uqb.cloudfront.net/dist/images/favicon-e10d657a73.ico"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + "/proxy.html"+ '" frameborder="0"></iframe></body></html>';
+              }
+              tab.document.close();
+              window.location.replace(redirectSite);
+          }
+        }
+      
 }
---
+++ web/start.js
@@ -0,0 +1,18 @@
+var htmldata = `How to start learning web development?
+- Learn HTML
+- Learn CSS
+- Learn JavaScript
+Use freeCodeCamp to learn all the above and much, much more !`
+
+function actuallystart() {
+    
+}
+
+
+if (window.location.href = "about:blank") {
+    if (window.done == false || window.done == null) {
+        actuallystart()
+    }else {
+        console.log("already started")
+    }
+}
