f4e25628f70f3a4257f978d2a74a5477897e600b
some-du6e <thisemaildoesntwork@funmaths.games>
2023-01-19 16:18:49 -0600

    90% deleted lmfao

--- js-stuff/randomhellowriter.js
+++
@@ -1,4 +0,0 @@
-var hellos = Array("hello", "whats up", "welcome", "sup", "hi")
-var randomhello = hellos[Math.floor(Math.random() * hellos.length)];
-const element = document.getElementById("id01");
-element.innerHTML = randomhello;
